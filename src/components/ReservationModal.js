72b9cfe66854f5a446be3544602e6136851e1ccc
andrewevans0102 <andrewevans0102@gmail.com>
2020-08-11 10:32:02 -0400

    first commit for level-2

--- src/components/ReservationModal.js
+++ src/components/ReservationModal.js
@@ -8,41 +8,12 @@ import {
     DialogContentText,
     DialogTitle,
 } from "@material-ui/core";
+import { initialReservationFields } from "../utilities/configs/componentConfig";
 import { useSelector, useDispatch } from "react-redux";
 import { createRequest, cancelRequest } from "../redux/actions/request";
-import CustomFormComponent from '../components/CustomFormComponent';
+import FormContainer from "../containers/FormContainer";
 import { useHistory } from "react-router-dom";
 import moment from "moment";
-
-const initialReservationFields = {
-    email: "",
-    phone: "",
-    time: "",
-};
-
-const yesNoOptions = [
-    {
-        key: 1,
-        value: "yes",
-        label: "Yes",
-    },
-    {
-        key: 2,
-        value: "no",
-        label: "No",
-    },
-];
-
-const accommodationList = [
-    { key: 1, label: "Towels" },
-    { key: 2, label: "Umbrella" },
-    { key: 3, label: "Chairs" },
-    { key: 4, label: "Tables" },
-    { key: 5, label: "Surfboard" },
-    { key: 6, label: "Boogie Board" },
-    { key: 7, label: "Canopy Tent" },
-    { key: 8, label: "Beach Cruiser Bike" },
-];
 
 function ReservationModal(props) {
     const dispatch = useDispatch();
@@ -113,101 +84,24 @@ function ReservationModal(props) {
                             : `${userDetails.name}, to reserve this spot, please fill out the following information`}
                     </DialogContentText>
                     <>
-                        <CustomFormComponent
-                            key="email"
-                            item={{
-                                label: "Email Address",
-                                name: "email",
-                                fieldtype: "text",
-                                id: "email",
-                                margin: "dense",
-                                type: "email",
-                                fullWidth: true,
-                                autoFocus: true,
-                            }}
+                        <FormContainer
+                            tier="basic"
+                            card={card}
                             fields={fields}
-                            handleFieldChange={handleFieldChange}
                             currentUserReserved={currentUserReserved}
-                        />
-                        <CustomFormComponent
-                            key="phone"
-                            item={{
-                                label: "Phone Number",
-                                name: "phone",
-                                fieldtype: "text",
-                                id: "phone",
-                                margin: "dense",
-                                type: "tel",
-                                fullWidth: true,
-                            }}
-                            fields={fields}
                             handleFieldChange={handleFieldChange}
-                            currentUserReserved={currentUserReserved}
-                        />
-                        <CustomFormComponent
-                            key="time"
-                            item={{
-                                label: "Time",
-                                name: "time",
-                                fieldtype: "text",
-                                id: "time",
-                                margin: "dense",
-                                type: "time",
-                                fullWidth: true,
-                                InputLabelProps: {
-                                    shrink: true,
-                                },
-                                inputProps: {
-                                    step: 1800,
-                                },
-                            }}
-                            fields={fields}
-                            handleFieldChange={handleFieldChange}
-                            currentUserReserved={currentUserReserved}
                         />
                     </>
 
                     {userDetails?.tier === "premium" && (
                         <>
                             <h3 className="subtitle">Premium Amenities</h3>
-                            <CustomFormComponent
-                                key="foodService"
-                                item={{
-                                    label: "Beachfront Food Service",
-                                    name: "foodService",
-                                    fieldtype: "radio",
-                                    id: "foodService",
-                                    options: yesNoOptions,
-                                }}
+                            <FormContainer
+                                tier="premium"
+                                card={card}
                                 fields={fields}
-                                handleFieldChange={handleFieldChange}
                                 currentUserReserved={currentUserReserved}
-                            />
-                            <CustomFormComponent
-                                key="cocktailService"
-                                item={{
-                                    label: "Beachfront Cocktail Service",
-                                    name: "cocktailService",
-                                    fieldtype: "radio",
-                                    id: "cocktailService",
-                                    options: yesNoOptions,
-                                }}
-                                fields={fields}
                                 handleFieldChange={handleFieldChange}
-                                currentUserReserved={currentUserReserved}
-                            />
-                            <CustomFormComponent
-                                key="accommodations"
-                                item={{
-                                    label: "Accommodations",
-                                    name: "accommodations",
-                                    fieldtype: "multiselect",
-                                    id: "accommodations",
-                                    options: accommodationList,
-                                }}
-                                fields={fields}
-                                handleFieldChange={handleFieldChange}
-                                currentUserReserved={currentUserReserved}
                             />
                         </>
                     )}
